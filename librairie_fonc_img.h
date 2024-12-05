cb0ddc6821b26dfb46e19f167fe1af23edc36657
matheo44 <64661992+matheo44@users.noreply.github.com>
2024-12-05 21:49:28 +0100

    Update librairie_fonc_img.h

--- librairie_fonc_img.h
+++ librairie_fonc_img.h
@@ -52,4 +52,40 @@ void assombrir_image(Image_PNG img, int val_assomb, int etape){
 	
 
 }
+//----------------------------------------------- Mathéo 05/12
+//bruiter image
 
+void bruiter_pixel(RVB& pixel){
+
+	int random_number;
+	random_number = rand() % 100- 9;
+	
+    if (pixel.rouge > pixel.rouge - random_number) {
+   	 pixel.rouge = static_cast<Composante>(pixel.rouge - random_number);
+    } 
+    
+    if (pixel.vert > pixel.vert - random_number) {
+   	 pixel.vert = static_cast<Composante>(pixel.vert - random_number);
+    } 
+    
+    if (pixel.bleu > pixel.bleu - random_number) {
+   	 pixel.bleu = static_cast<Composante>(pixel.bleu - random_number);
+    } 
+    
+}
+
+
+
+void bruiter_image(Image_PNG img, int etape){
+	//Variables
+	int i, j;
+	//Début
+	for (i = 0 ; i < static_cast<int>(img.largeur) ; ++i){
+    		for (j = 0 ; j < static_cast<int>(img.hauteur) ; ++j){
+        		bruiter_pixel(img.pixels[j][i]);
+    	}
+	}
+	sauver_PNG("./result/"+std::to_string(etape)+".png", img);  
+	
+
+}
